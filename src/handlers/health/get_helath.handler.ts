b562fed3a19116d15906c5565d46f23aea38d51a
vpetrov-qb <vpetrov@quickbase.com>
2025-11-29 23:41:39 +0200

    Rename getHealth to getHealthHandler for consistency and clarity

--- src/handlers/health/get_helath.handler.ts
+++ src/handlers/health/get_helath.handler.ts
@@ -1,6 +1,7 @@
 import type { RouteHandler } from "../../types/RouteHnadler.ts";
 import json from "../../utils/routes/json.ts";
 
-const getHealth: RouteHandler = () => json({ message: "Service is healthy" });
+const getHealthHandler: RouteHandler = () =>
+    json({ message: "Service is healthy" });
 
-export default getHealth;
+export default getHealthHandler;
